1d60195d237ee7308588fd88e20f95f2045c6158
Kunal Kumar Jha <kunatastic@gmail.com>
2022-06-16 14:36:03 +0530

    fix: nav bar link

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -4,8 +4,8 @@ import { Link, useLocation } from 'react-router-dom';
 function Nav() {
   const NavLinks = [
     { href: '/', text: 'Home' },
-    { href: '/join-room', text: 'Join Room' },
-    { href: '/create-room', text: 'Create Room' },
+    { href: '/join-room/', text: 'Join Room' },
+    { href: '/create-room/', text: 'Create Room' },
   ];
   const location = useLocation();
 
